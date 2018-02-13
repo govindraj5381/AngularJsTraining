5e96bbb7de6d1db6165a1bfe0555ec2309523bee
govindraj5381 <govindraj673@gmail.com>
2018-02-13 00:52:55 -0800

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 var myapp = angular.module('myapp', [])
-console.log(myapp)
+//console.log(myapp)
+
+myapp.controller('Myctrl',['$scope','AppService', function($scope, AppService){
+    $scope.myval = "Hello";
+    
+ AppService.getStudents().then(function(result)
+{
+    $scope.students=result.data.map(function(item){
+        item.seniority = item.age > 10 ? "S" : "J";
+        return item; 
+    });
+})
+    $scope.onViewBtnClick = function(student){
+        //alert("button clicked")
+
+        //$scope.myval ="welcome"
+        $scope.selectedstudent = student
+    }
+}])
---
+++ app.service.js
@@ -0,0 +1,11 @@
+var myapp = angular.module('myapp')
+
+myapp.service('AppService', ['$http',function($http){
+
+ 
+    this.getStudents = function()
+    {
+         return $http.get("/students.json");
+        //return students;
+    }
+}]);
