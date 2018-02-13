e4dd749d03506b1dd3c71845395d5bd0c01bc6d5
govindraj5381 <govindraj673@gmail.com>
2018-02-13 03:44:57 -0800

    bindings

---
+++ app.component.js
@@ -0,0 +1,11 @@
+var myapp = angular.module('myapp')
+
+myapp.component('studentDetails',{ 
+templateUrl : 'student-details.tpl.html',
+bindings :{
+    myTitle : '@',
+    currentStudent : '<'
+},
+controller : [function(){
+    this.myname = "custom component"
+}]});
